9652b40994260e79d13d76f01c134b95607afddb
fabillar <luv_fabz@yahoo.com>
2016-01-28 02:35:41 +0800

    Create pmpro_memberships_posts.php
    Parameters:
    $level_id = Membership Level ID / (int) required
    
    Sample usage:
    <?php echo pmpro_memberships_posts($level_id); ?>

---
+++ pmpro_memberships_posts.php
@@ -0,0 +1,30 @@
+// FUNCTION TO GET ALL POST IDs ASSOCIATED WITH A LEVEL
+// List ALL posts a defined Level ID gives access to...
+
+function pmpro_memberships_posts($level_id) {
+	global $wpdb;
+	$pmpro_memberships_pages = $wpdb->prefix .'pmpro_memberships_pages';
+
+	$sqlQuery = $wpdb->prepare("SELECT page_id FROM {$pmpro_memberships_pages} WHERE membership_id = %d", $level_id);
+	$pmproPost = $wpdb->get_results($sqlQuery);
+	
+  	// DETERMINE IF THIS LEVEL IS ASSOCIATED TO A POST or POSTS...
+	if (!empty(array_filter($pmproPost))) {
+	
+	  	// IF YES, POST IDs ARE FOUND...
+		foreach($pmproPost as $post) {
+
+		  	// MAKE SURE POSTS ARE PUBLISHED...
+			if (get_post_status($post->page_id) == 'publish') {
+
+				// MODIFY OUTPUT AS DESIRED...
+				echo  "&bull; Course : <a href='" . get_the_permalink($post->page_id) . "'>" . get_the_title($post->page_id) . "</a><br>";
+				
+			}
+		}
+		
+	} else {
+	  	// IF NO POST IDs WERE FOUND (NO POSTS ASSOCIATED TO THIS LEVEL)...
+		echo "This Membership Level is currently not associated with any available posts.";
+	}
+}
