e5ef649751b1d9f88e40f3e40721151d88800626
fabillar <luv_fabz@yahoo.com>
2016-01-28 15:40:07 +0800

    Update pmpro_get_posts_from_membership_level_id.php

--- pmpro_get_posts_from_membership_level_id.php
+++ pmpro_get_posts_from_membership_level_id.php
@@ -7,6 +7,7 @@ function pmpro_memberships_posts($level_id) {
 	if($level_id) {
 		global $wpdb;
 		$pmpro_memberships_pages = $wpdb->prefix .'pmpro_memberships_pages';
+		$disallowed_posts = array("trash", "inherit", "draft", "auto-draft"); // POSTS NOW TO SHOW
 
 		$sqlQuery = $wpdb->prepare("SELECT page_id FROM {$pmpro_memberships_pages} WHERE membership_id = %d", $level_id);
 		$pmproPost = $wpdb->get_results($sqlQuery);
@@ -14,7 +15,7 @@ function pmpro_memberships_posts($level_id) {
 	  	// DETERMINE IF THIS LEVEL IS ASSOCIATED TO A POST or POSTS...
 		if (!empty(array_filter($pmproPost))) {
 		
-		  	// IF YES, POST IDs ARE FOUND...
+		  	// IF YES, POST ID/IDs ARE FOUND...
 			foreach($pmproPost as $post) {
 
 			  	// MAKE SURE POSTS ARE PUBLISHED...
@@ -26,12 +27,15 @@ function pmpro_memberships_posts($level_id) {
 					
 				} else {
 
-					// UNPUBLISHED POSTS ASSOCIATED TO THIS LEVEL...
-					// (You can choose not to show this)
-					echo "&bull; " . $post->status . " : " . get_the_title($post->page_id) . "</br>";
+					// CHECK FOR OTHER ALLOWED POSTS STATUSES, ASIDE FROM "PUBLISHED"...
+					if (!in_array(get_post_status($post->page_id), $disallowed_posts)) {
+
+						// UNPUBLISHED POSTS ASSOCIATED TO THIS LEVEL...
+						// (If you want to show them, e.g. Pending, Future, Private posts...)
+						echo "&bull; " . ucwords(get_post_status($post->page_id)) . " : " . get_the_title($post->page_id) . "</br>";
+					}
 				}
 			}
-			
 		} else {
 		  	// IF NO POST IDs WERE FOUND (NO POSTS ASSOCIATED TO THIS LEVEL)...
 			echo "This Membership Level is currently not associated with any available posts.";
